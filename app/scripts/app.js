5b3639cdb90cac7db3d0934a75f9519f2097fefd
Zach Dunn <zach@onemightyroar.com>
2013-08-20 16:05:30 -0400

    Updated to use angular-camera bower component

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angularToolkitApp', [])
+angular.module('angularToolkitApp', ['omr.directives'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
